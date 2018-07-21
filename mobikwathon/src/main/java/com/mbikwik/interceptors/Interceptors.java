f96cd990031294be91b7b0086b7e5a4b2f06b6f0
himanshu-1007 <himanshu.mittal@mobikwik.com>
2018-07-21 15:14:57 +0530

    Defining viewsresolver

---
+++ mobikwathon/src/main/java/com/mbikwik/interceptors/Interceptors.java
@@ -0,0 +1,17 @@
+package com.mbikwik.interceptors;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class Interceptors extends HandlerInterceptorAdapter {
+
+
+
+
+
+
+
+
+
+
+
+}
